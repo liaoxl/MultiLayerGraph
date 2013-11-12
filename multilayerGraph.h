b53d65766883c40be82a8396189c88c5ae43a6f1
Xiangli Liao <liaoxl2012@gmail.com>
2013-11-12 19:42:10 +0800

    Origin Graphcuts

---
+++ multilayerGraph.h
@@ -0,0 +1,17 @@
+/*
+ * multilayerGraph.h
+ * Copyright (C) 2013 moondark <liaoxl2012@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#ifndef __MULTILAYERGRAPH_H__
+#define __MULTILAYERGRAPH_H__
+
+#include "graph.h"
+
+
+
+
+#endif /* !__MULTILAYERGRAPH_H__ */
+
